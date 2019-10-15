900d8716a3fa8ff10e0988c174a06b22cbf49248
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-10-14 17:28:24 -0700

    init

---
+++ src/HomePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+export default class HomePage extends Component {
+  render(){
+    return(
+      <div className="homepage">
+        <article aria-label="A favorite quote">
+    <quote>“I may not have gone where I intended to go, but I think I have ended up where I needed to be.” 
+      ― douglas adams, The Long Dark Tea-Time of the Soul</quote>
+  </article>
+
+      </div>
+    );
+  }
+
+
+}
