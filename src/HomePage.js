5648ef06865c076caa1c1333b6004dd72c1c3de4
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-11-02 11:18:28 -0700

    fixed some typos

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,7 +10,7 @@ export default class HomePage extends Component {
       <div className="homepage">
         <article className="AdamsQuote" aria-label="A favorite quote">
     <p>“I may not have gone where I intended to go, but I think I have ended up where I needed to be.” 
-      ― douglas adams, The Long Dark Tea-Time of the Soul</p>
+      ― Douglas Adams, The Long Dark Tea-Time of the Soul</p>
   </article>
   <div className="heroImgContainer">
   <img className="hero" src={hero} alt='Portland Oregon Rail bridge, lit for dusk' />
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,11 +8,13 @@ export default class About extends Component {
         <article className="bio" arial-label="biography section">
           <h2 className="aboutTitle">A little about me</h2>
 
-          <p className="aboutText"> The code school I chose, Thinkful, taught me that we never stop growing or learning. When I'm not finding new drills or code challenges to work, I spend my time in a variety of ways. Living in Portland means access to world class soccer, art, nature, and farmers markets.</p>
+          <p className="aboutText"> The code school I chose, Thinkful, taught me that we never stop growing or learning. When I'm not finding new drills or code challenges to work, I spend my time in a variety of ways. Living in Portland means access to world class soccer, art, nature, and farmers' markets.</p>
           <p className="aboutText">
-          As a Timbers and Thorns fan, I joined the 107ist Supporters group. I spend some of my time giving back to the community through volunteer activities that support the growth of soccer and equality throughout our area.The 107ist supporters pull together as a team, which is something I've found I enjoy taking part in during my experiance with Thinkful's Enigneering Immersion team approach. </p>
+          As a Timbers and Thorns fan, I joined the 107ist Supporters group. I spend some of my time giving back to the community through volunteer activities that support the growth of soccer and equality throughout our area. The 107ist supporters pull together as a team, which is something I've found I enjoy taking part in during my experience with Thinkful's Engineering Immersion team approach. </p><br/>
           <p className="aboutText">
           Programming, especially with an eye towards making things accessible to all, is my current passion. Working with a variety of people in class has helped me find the things I'm good at doing and taught me that it is okay to not know the answer right away. The great thing about tech and me is that we both grow and change with time. Seems like I found my niche.</p>
+          <br />
+          <p>My primary tech stack from Thinkful includes HTML, JS, CSS, React, Node.js, Express, PostgreSQL. I'm looking forward to expanding on that knowledge and learning more languages.</p>
         </article>
       </div>
     );
