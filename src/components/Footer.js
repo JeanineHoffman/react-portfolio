74ef171215d71cdcbf7c7854acf52a6471de4778
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-11-03 15:24:58 -0800

    repaired links to have target blank

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,23 @@ export default class Footer extends Component {
     return (
       <div className='footerWrapper'>
         <div className="logoFooter">
-        <Link
-          to='/'><img src={logo} className="logo" alt='logo-gold background with a douglas fir tree in black on the left and the initials JH on the right' /></Link>
-          </div>
-          <div className="copyright">
-        <p >Jeanine Hoffman © 2019</p>
-      </div>
-      <div className="footerContact">
-        <a href="mailto:jeaninehoffman42@gmail.com">
-          <img className="footerLink" src={email} alt="email link" target="_blank" rel="noopener noreferrer"/></a>
-        <a href="https://github.com/JeanineHoffman">
-          <img className="footerLink" src={github} alt="Github link" target="_blank" rel="noopener noreferrer" /></a>
-        <a href="https://www.linkedin.com/in/jeanine-hoffman42">
-          <img className="footerLink" src={linkedin} alt="LinkedIn link" target="_blank" rel="noopener noreferrer" /></a>
-      </div>
+          <Link
+            to='/'><img src={logo} className="logo" alt='logo-gold background with a douglas fir tree in black on the left and the initials JH on the right' /></Link>
+        </div>
+        <div className="copyright">
+          <p >Jeanine Hoffman © 2019</p>
+        </div>
+        <div className="footerContact">
+          <a href="mailto:jeaninehoffman42@gmail.com" target="_blank" rel="noopener noreferrer">
+
+            <img className="footerLink" src={email} alt="email link" /></a>
+
+          <a href="https://github.com/JeanineHoffman" target="_blank" rel="noopener noreferrer">
+            <img className="footerLink" src={github} alt="Github link" /></a>
+            
+          <a href="https://www.linkedin.com/in/jeanine-hoffman42" target="_blank" rel="noopener noreferrer">
+            <img className="footerLink" src={linkedin} alt="LinkedIn link" /></a>
+        </div>
       </div>
     );
   }
